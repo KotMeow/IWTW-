555c6a339620a9c5bb6beaa34d43f0e72c6f56aa
Kamil Kot <kaamil.kot@gmail.com>
2016-10-22 01:41:03 +0200

    Delete invalid import

--- src/main/java/com/example/iwtw/service/StorageService.java
+++ src/main/java/com/example/iwtw/service/StorageService.java
@@ -1,9 +1,6 @@
 package com.example.iwtw.service;
 
 import com.example.iwtw.domain.Movie;
-import com.sun.xml.internal.ws.util.StringUtils;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
